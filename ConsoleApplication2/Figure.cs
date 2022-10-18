d442a41d4268a0b23e49f9480270cb5fd4a614b5
DLMTV <domkrat90919@gmail.com>
2022-10-18 21:07:54 +0300

    Ploshadi_Figur_DZ

---
+++ ConsoleApplication2/Figure.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Runtime.Remoting.Messaging;
+using ConsoleApplication2;
+
+namespace ConsoleApplication2
+{
+    public abstract class Figure
+    {
+        public abstract double Ploshad();
+        
+
+        public class Triangle : Figure
+        {
+            double a;
+            double b;
+            double c;
+
+            public Triangle(double a, double b, double c)
+            {
+                this.a = a;
+                this.b = b;
+                this.c = c;
+            }
+            public override double Ploshad()
+            {
+                double p = ((a + b + c) / 2);
+                return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            }
+        }
+        
+        public class Square : Figure
+        {
+            double d;
+
+            public Square(double d)
+            {
+                this.d = d;
+            }
+
+            public override double Ploshad()
+            {
+                return d * d;
+            }
+            /*S =a* a */
+
+        }
+
+        public class Circle : Figure
+        {
+            double r;
+
+            public Circle(double r)
+            {
+                this.r = r;
+            }
+
+            public override double Ploshad()
+            {
+                return 3.14 * (r * r);
+
+                /*  s = pi*(r*r)*/
+            }
+        }
+    }
+}
---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,57 @@
+﻿
+using System;
+
+namespace ConsoleApplication2
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            while (true)
+            {
+                Console.Clear();
+                
+                Figure f1, f2, f3;
+                    Console.WriteLine("Вы можете рассчитать площади трёх фигур: 1 - Треугольник; 2 - Квадрат; 3 - Круг.");
+                    Console.WriteLine("Выберите фигуру для рассчёта площади:");
+                    var Fig = (Console.ReadLine());
+                    switch (Fig)
+                    {
+                        case ("1"):
+                            Console.WriteLine("Треугольник");
+                            Console.Write("Введите длину стороны А = ");
+                            int a = Convert.ToInt32(Console.ReadLine());
+                            Console.Write("Bведите длину стороны B = ");
+                            int b = Convert.ToInt32(Console.ReadLine());
+                            Console.Write("Bведите длину стороны C = ");
+                            int c = Convert.ToInt32(Console.ReadLine());
+                            f1 = new Figure.Triangle(a, b, c);
+                            Console.Write("Площадь треугольника = " + f1.Ploshad() + "мм^2");
+                            break;
+
+                        case ("2"):
+                            Console.WriteLine("Квадрат");
+                            Console.Write("Введите длину стороны А = ");
+                            int d = Convert.ToInt32(Console.ReadLine());
+                            f2 = new Figure.Square(d);
+                            Console.WriteLine("Площадь квадрата = " + f2.Ploshad()+"мм^2");
+                            break;
+                        
+                        case ("3"):
+                            Console.WriteLine("Круг");
+                            Console.Write("Введите радиус R = ");
+                            int r = Convert.ToInt32(Console.ReadLine());
+                            f3 = new Figure.Circle(r);
+                            Console.WriteLine("Площадь круга = " + f3.Ploshad()+"мм^2");
+                            break;
+                        default:
+                            Console.WriteLine("Введены некорректные данные!!");
+                            break;
+                    }
+                    
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
